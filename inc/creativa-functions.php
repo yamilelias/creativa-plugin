65664d6da78553d40855db615c6d73d892bf23ce
yamilelias <yamileliassoto@gmail.com>
2017-12-07 13:56:00 -0700

    Show content page instead of the default slider in homepage

--- inc/creativa-functions.php
+++ inc/creativa-functions.php
@@ -21,3 +21,13 @@ function is_woocommerce_activated() {
 function creativa_styles_and_scripts() {
     wp_enqueue_style('creativa', plugins_url( 'assets/css/creativa.css', dirname(__FILE__) ));
 }
+
+/**
+ * Display the content of the set page in the homepage.
+ *
+ * @since 1.0.0
+ */
+function creativa_homepage_content() {
+    $post = get_post();
+    echo $post->post_content;
+}
--- inc/creativa-hooks.php
+++ inc/creativa-hooks.php
@@ -7,3 +7,6 @@
  */
 
 add_action('wp_enqueue_scripts', 'creativa_styles_and_scripts', 90);
+
+remove_action('storevilla_homepage', 'storevilla_main_slider', 10);
+add_action('storevilla_homepage', 'creativa_homepage_content', 10);
