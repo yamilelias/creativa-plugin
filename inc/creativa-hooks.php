0d5a0487df9938e8820e9f4264711df06f47f7cf
yamilelias <yamileliassoto@gmail.com>
2017-12-11 12:51:26 -0700

    Fixed header showing

--- inc/creativa-hooks.php
+++ inc/creativa-hooks.php
@@ -8,6 +8,9 @@
 
 add_action('wp_enqueue_scripts', 'creativa_styles_and_scripts', 90);
 
+remove_action('storevilla_header', 'storevilla_button_header', 20);
+add_action('storevilla_header', 'creativa_branding_header', 20);
+
 remove_action('storevilla_homepage', 'storevilla_main_slider', 10);
 remove_action('storevilla_homepage', 'storevilla_brand_logo',  30);
 add_action('storevilla_homepage', 'creativa_homepage_content', 10);
--- inc/creativa-template-functions.php
+++ inc/creativa-template-functions.php
@@ -7,6 +7,58 @@
  */
 
 /**
+ * Display Site Branding
+ * @since  1.0.0
+ * @return void
+ */
+function creativa_branding_header() { ?>
+    <div class="header-wrap clearfix">
+        <div class="store-container">
+            <div class="site-branding">
+                <?php
+                if ( has_custom_logo() ) {
+                    the_custom_logo();
+                } else {
+                ?>
+                    <div class="sv-logo-wrap">
+                        <div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
+                        <?php
+                        $description = get_bloginfo( 'description', 'display' );
+                        if ( $description || is_customize_preview() ) :
+                            ?>
+                            <p class="site-description"><?php echo $description; ?></p>
+                        <?php endif; ?>
+                    </div>
+                <?php } ?>
+            </div><!-- .site-branding -->
+            <div class="search-cart-wrap clearfix">
+                <?php
+
+                /**
+                 * Display Product Search
+                 * @since  1.0.0
+                 * @uses  storevilla_is_woocommerce_activated() check if WooCommerce is activated
+                 * @return void
+                 */
+
+                if ( storevilla_is_woocommerce_activated() ) { ?>
+                    <div class="advance-search">
+                        <?php storevilla_product_search(); ?>
+                    </div>
+                <?php } else{ ?>
+                    <div class="normal-search">
+                        <?php get_search_form(); ?>
+                    </div>
+                <?php } ?>
+
+            </div>
+        </div>
+    </div>
+
+    <?php
+}
+
+/**
  * Display the theme credit
  * @since  1.0.0
  * @return void
